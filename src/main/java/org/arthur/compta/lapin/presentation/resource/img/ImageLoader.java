ffb118b35eaf527db1aae8fdcd0187cc358bb1e8
arzman <arz.man@free.fr>
2017-02-28 11:12:15 +0100

    Ajout d'un utilitaire de chargement d'image

---
+++ src/main/java/org/arthur/compta/lapin/presentation/resource/img/ImageLoader.java
@@ -0,0 +1,16 @@
+package org.arthur.compta.lapin.presentation.resource.img;
+
+import javafx.scene.image.Image;
+
+public class ImageLoader {
+	
+	
+	
+	public static Image getImage(String nom){
+		
+		return new Image("org/arthur/compta/lapin/presentation/resource/img/"+nom);
+		
+	}
+	
+
+}

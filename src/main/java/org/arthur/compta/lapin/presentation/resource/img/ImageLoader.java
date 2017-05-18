29dd1ff33ac0b049f54271a8d589cd1f68e18a2f
arzman <arz.man@free.fr>
2017-05-18 13:51:28 +0200

    Des comz sur ImageLoader

--- src/main/java/org/arthur/compta/lapin/presentation/resource/img/ImageLoader.java
+++ src/main/java/org/arthur/compta/lapin/presentation/resource/img/ImageLoader.java
@@ -2,9 +2,13 @@ package org.arthur.compta.lapin.presentation.resource.img;
 
 import javafx.scene.image.Image;
 
+/**
+ * S'occupe du chargement des images dans l'application
+ *
+ */
 public class ImageLoader {
 
-	// un plus
+	// un plus vert
 	public static final String ADD_IMG = "add_icon.gif";
 	// une croix rouge
 	public static final String DEL_IMG = "remove_icon.gif";
@@ -12,6 +16,11 @@ public class ImageLoader {
 	public static final String EDIT_IMG = "edit_icon.jpg";
 	
 	
+	/**
+	 * Retourne une image
+	 * @param nom le nom de l'image a retourner
+	 * @return l'image
+	 */
 	public static Image getImage(String nom) {
 
 		return new Image("org/arthur/compta/lapin/presentation/resource/img/" + nom);

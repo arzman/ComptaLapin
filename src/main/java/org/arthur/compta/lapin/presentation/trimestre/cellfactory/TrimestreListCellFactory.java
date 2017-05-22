32da36183957bf305454cc029b452872a9c281e7
arzman <arz.man@free.fr>
2017-05-22 10:35:36 +0200

    Sélection du trimestre courant

---
+++ src/main/java/org/arthur/compta/lapin/presentation/trimestre/cellfactory/TrimestreListCellFactory.java
@@ -0,0 +1,72 @@
+package org.arthur.compta.lapin.presentation.trimestre.cellfactory;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.arthur.compta.lapin.presentation.utils.ApplicationFormatter;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+/**
+ * Callback permettant de personaliser l'affichage de l'ihm de sélection du
+ * trimestre courant. Pour chaque trimestre ( l'id du trimestre) on affichage sa
+ * date de début et fin.
+ *
+ */
+public class TrimestreListCellFactory implements Callback<ListView<String>, ListCell<String>> {
+
+	/**
+	 * Map permettant de liée l'id du trimstre à sa date de début
+	 */
+	private HashMap<String, Calendar> _resumeTrimestre;
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param resumeTrimestre
+	 *            les id des trimestres et leur date de début
+	 */
+	public TrimestreListCellFactory(HashMap<String, Calendar> resumeTrimestre) {
+
+		_resumeTrimestre = resumeTrimestre;
+
+	}
+
+	/**
+	 * @see javafx.util.Callback#call(java.lang.Object)
+	 */
+	@Override
+	public ListCell<String> call(ListView<String> param) {
+
+		ListCell<String> cell = new ListCell<String>() {
+
+			@Override
+			protected void updateItem(String item, boolean empty) {
+
+				super.updateItem(item, empty);
+
+				if (!empty) {
+
+					// De <mois année de début> à < mois année de fin>
+					StringBuilder text = new StringBuilder("De ");
+					Calendar deb = _resumeTrimestre.get(item);
+					text.append(ApplicationFormatter.moiAnneedateFormat.format(deb.getTime()));
+					text.append(" à ");
+					Calendar fin = Calendar.getInstance();
+					fin.setTime(new Date(deb.getTime().getTime()));
+					fin.add(Calendar.MONTH, 3);
+					text.append(ApplicationFormatter.moiAnneedateFormat.format(fin.getTime()));
+					setText(text.toString());
+
+				}
+
+			}
+		};
+
+		return cell;
+	}
+
+}
--- src/main/java/org/arthur/compta/lapin/presentation/trimestre/dialog/SelectTrimestreDialog.java
+++ src/main/java/org/arthur/compta/lapin/presentation/trimestre/dialog/SelectTrimestreDialog.java
@@ -6,45 +6,101 @@ import java.util.HashMap;
 import org.arthur.compta.lapin.application.exception.ComptaException;
 import org.arthur.compta.lapin.application.manager.TrimestreManager;
 import org.arthur.compta.lapin.presentation.exception.ExceptionDisplayService;
+import org.arthur.compta.lapin.presentation.trimestre.cellfactory.TrimestreListCellFactory;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 
+/**
+ * Fenêtre de sélection d'un trimestre : affiche la date de début/fin du
+ * trimestre
+ *
+ */
 public class SelectTrimestreDialog extends Dialog<String> {
-	
-	
-	
+
+	/**
+	 * Les id des trimestre à afficher ainsi que leur date de début.
+	 */
+	private HashMap<String, Calendar> _resumeTrimestre;
+	/**
+	 * Affichage de la liste des trimestres
+	 */
+	private ListView<String> _listV;
+
 	public SelectTrimestreDialog() {
-		
+
 		setTitle("Sélection du trimestre courant");
-		
+
 		// création de la zone de sélection
-		GridPane contente = new GridPane();
-		getDialogPane().setContent(contente);
-		
+		GridPane content = new GridPane();
+		getDialogPane().setContent(content);
+
 		try {
-			// récupératio des trimestres de l'application ainsi que leur date de début
-			HashMap<String,Calendar> resumeTrimestre = TrimestreManager.getInstance().getAllTrimestreShortList();
-			
-			//TODO To be contined...
-			ObservableList<String> cont = FXCollections.observableArrayList();
-			
-			cont.addAll(resumeTrimestre.keySet());
-			
-			ListView<String> listV = new ListView<>();
-			listV.setItems(cont);
-			
-			contente.add(listV, 0, 0);
-			
-			
+			// récupération des trimestres de l'application ainsi que leur date
+			// de début
+			_resumeTrimestre = TrimestreManager.getInstance().getAllTrimestreShortList();
+
+			// Création de la liste des trimestres à afficher
+			ObservableList<String> trimidlist = FXCollections.observableArrayList();
+			trimidlist.addAll(_resumeTrimestre.keySet());
+
+			// affichage de la liste
+			_listV = new ListView<>();
+			_listV.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+			// callback permettant de customiser l'affichage
+			_listV.setCellFactory(new TrimestreListCellFactory(_resumeTrimestre));
+
+			_listV.setItems(trimidlist);
+			content.add(_listV, 0, 0);
+
 		} catch (ComptaException e) {
 			ExceptionDisplayService.showException(e);
 		}
-		
-		
+
+		// création des boutons
+		createBoutonBar();
+
+	}
+
+	/**
+	 * Crée les boutons OK et Cancel
+	 */
+	private void createBoutonBar() {
+
+		// Création du bouton OK
+		ButtonType _buttonTypeOk = new ButtonType("Ok", ButtonData.OK_DONE);
+		getDialogPane().getButtonTypes().add(_buttonTypeOk);
+
+		// Création du bouton Cancel
+		ButtonType buttonTypeCancel = new ButtonType("Annuler", ButtonData.CANCEL_CLOSE);
+		getDialogPane().getButtonTypes().add(buttonTypeCancel);
+
+		// Retourne le Compte créé sur le OK
+		setResultConverter(new Callback<ButtonType, String>() {
+
+			@Override
+			public String call(ButtonType param) {
+
+				String zeReturn = null;
+
+				// appuie sur Ok : on crée le trimestre
+				if (param.getButtonData().equals(ButtonData.OK_DONE)) {
+
+					// recup du trimestre
+					zeReturn = _listV.getSelectionModel().getSelectedItem();
+
+				}
+
+				return zeReturn;
+			}
+		});
 	}
 
 }
